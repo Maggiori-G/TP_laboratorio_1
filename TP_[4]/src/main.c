e83b14d85724b9ceeb812be674a77f716b51cebb
Gianni Maggiori <taniitoo@gmail.com>
2021-11-20 21:14:07 -0300

    entrega tp 4_ linkedList

---
+++ TP_[4]/src/main.c
@@ -0,0 +1,82 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2018>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../testing/inc/main_test.h"
+#include "../inc/LinkedList.h"
+
+
+int main(void){
+//	startTesting(1);  // ll_newLinkedList                //listo
+//	startTesting(2);  // ll_len							 //listo
+//	startTesting(3);  // getNode - test_getNode  		 //listo
+//	startTesting(4);  // addNode - test_addNode			 //listo
+//	startTesting(5);  // ll_add							 //listo
+//	startTesting(6);  // ll_get							 //listo
+//	startTesting(7);  // ll_set							 //listo
+//	startTesting(8);  // ll_remove						 //listo
+//	startTesting(9);  // ll_clear						 //listo
+//	startTesting(10); // ll_deleteLinkedList			 //listo
+//	startTesting(11); // ll_indexOf						 //listo
+//	startTesting(12); // ll_isEmpty						 //listo
+//	startTesting(13); // ll_push						 //listo
+//	startTesting(14); // ll_pop							 //listo
+//	startTesting(15); // ll_contains					 //listo
+//	startTesting(16); // ll_containsAll					 //listo
+//	startTesting(17); // ll_subList						 //listo
+//	startTesting(18); // ll_clone						 //listo
+//	startTesting(19); // ll_sort						 //listo
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
